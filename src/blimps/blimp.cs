b01b96fd53199b6d62dedbd11e8eb9ccc55c73e6
ConanODoyle <samuel.w.sze@gmail.com>
2020-02-02 11:20:24 -0800

    Complete basic blimp model, movement functions, and movement tool

--- src/blimps/blimp.cs
+++ src/blimps/blimp.cs
@@ -3,7 +3,7 @@ datablock PlayerData(BlimpArmor : PlayerStandardArmor)
 	shapeFile = "./blimp.dts";
 	uiName = "";
 
-	boundingBox = vectorScale("2 2 2", 4);
+	boundingBox = vectorScale("2 2 2.8", 4);
 };
 
 if (!isObject(BlimpSimSet))
@@ -15,14 +15,14 @@ function getBlimp()
 {
 	%shape = new AIPlayer(Blimps)
 	{
-		dataBlock = BlimpArmorData;
+		dataBlock = BlimpArmor;
 		isBlimp = 1;
 	};
 	$BlimpSimSet.add(%shape);
 	return %shape;
 }
 
-function BlimpArmor::moveBlimp(%blimp, %destination)
+function AIPlayer::moveBlimp(%blimp, %destination)
 {
 	if (!%blimp.isBlimp)
 	{
@@ -32,9 +32,14 @@ function BlimpArmor::moveBlimp(%blimp, %destination)
 	%blimp.setAimLocation(%destination);
 	%blimp.setMoveY(1);
 	%blimp.distanceCheckLoop(%destination);
+	if (isObject(%blimp.moveShape))
+	{
+		%blimp.moveShape.delete();
+	}
+	%blimp.moveShape = createRingMarker(%destination, "0 1 0 1", "1 1 0.1");
 }
 
-function BlimpArmor::distanceCheckLoop(%blimp, %destination)
+function AIPlayer::distanceCheckLoop(%blimp, %destination)
 {
 	cancel(%blimp.distanceCheckLoop);
 	%dist = vectorDist(%blimp.getPosition(), %destination);
@@ -43,10 +48,72 @@ function BlimpArmor::distanceCheckLoop(%blimp, %destination)
 	{
 		%blimp.setMoveY(0);
 		%blimp.clearAim();
+		%blimp.moveShape.delete();
 		return;
 	}
 
 	%blimp.lastDistanceCheck = %dist;
 
 	%blimp.distanceCheckLoop = %blimp.schedule(33, distanceCheckLoop, %destination);
+}
+
+function testAim(%pl, %bot)
+{
+	if (!isObject(%pl) || !isObject(%bot))
+	{
+		return;
+	}
+
+	cancel(%bot.testAimSched);
+
+	%bot.setAimVector(%pl.getForwardVector());
+	%bot.testAimSched = schedule(33, %bot, testAim, %pl, %bot);
+}
+
+function serverCmdGetBalloon(%cl)
+{
+	if (isObject(%cl.balloon))
+	{
+		%cl.balloon.delete();
+	}
+
+	if (!isObject(%cl.player))
+	{
+		return;
+	}
+	%cl.balloon = getBlimp();
+	%cl.balloon.maxyawspeed = 0.8;
+	%cl.balloon.setMoveY(0.2);
+	%cl.balloon.setTransform(%cl.player.getTransform());
+	%cl.balloon.setShapeName(%cl.name @ "'s Balloon", 8564862);
+	%cl.balloon.setNodeColor("ALL", %cl.chestcolor);
+
+	testAim(%cl.player, %cl.balloon);
+}
+
+function serverCmdTestMoveBalloon(%cl)
+{
+	if (!isObject(%cl.balloon))
+	{
+		return;
+	}
+
+	cancel(%cl.balloon.testAimSched);
+	%cl.balloon.setMoveY(0);
+
+	// talk("moving");
+	if (isObject(%cl.player))
+	{
+		%pl = %cl.player;
+		%start = %pl.getEyeTransform();
+		%end = vectorAdd(vectorScale(%pl.getEyeVector(), 200), %start);
+		%masks = $Typemasks::fxBrickObjectType | $Typemasks::StaticObjectType | $Typemasks::StaticShapeObjectType | $Typemasks::EnvironmentObjectType;
+		%ray = containerRaycast(%start, %end, %masks);
+		if (isObject(%hit = getWord(%ray, 0)))
+		{
+			%hitloc = getWords(%ray, 1, 3);
+			// talk(%hitloc);
+			%cl.balloon.moveBlimp(%hitloc);
+		}
+	}
 }
---
+++ src/blimps/tools.cs
@@ -0,0 +1,65 @@
+datablock ShapeBaseImageData(BlimpControlImage)
+{
+	shapeFile = printGunImage.shapefile;
+	emap = 1;
+
+	doColorShift = 1;
+	colorShiftColor = "1 0 0 1";
+
+	armReady = 1;
+
+	mountPoint = 0;
+
+	stateName[0] = "Activate";
+	stateTimeoutValue[0] = 0.1;
+	stateTransitionOnTimeout[0]	= "Ready";
+
+	stateName[1] = "Ready";
+	stateTransitionOnTriggerDown[1] = "Fire";
+
+	stateName[2] = "Fire";
+	stateScript[2] = "onFire";
+	stateTimeoutValue[2] = 0.1;
+	stateTransitionOnTimeout[2] = "Refire";
+
+	stateName[3] = "Refire";
+	stateTransitionOnTriggerDown[3] = "Fire";
+	stateTransitionOnTriggerUp[3] = "Ready";
+};
+
+function BlimpControlImage::onFire(%this, %obj, %slot)
+{
+	%cl = %obj.client;
+	if (!isObject(%cl.balloon))
+	{
+		%cl.centerprint("You don't have a balloon!", 1);
+		return;
+	}
+
+	cancel(%cl.balloon.testAimSched);
+	%cl.balloon.setMoveY(0);
+
+	if (isObject(%cl.player))
+	{
+		%pl = %cl.player;
+		%start = %pl.getEyeTransform();
+		%end = vectorAdd(vectorScale(%pl.getEyeVector(), 50), %start);
+		%masks = $Typemasks::fxBrickObjectType | $Typemasks::StaticObjectType | $Typemasks::StaticShapeObjectType | $Typemasks::EnvironmentObjectType;
+		%ray = containerRaycast(%start, %end, %masks);
+		if (isObject(%hit = getWord(%ray, 0)))
+		{
+			%hitloc = getWords(%ray, 1, 3);
+			if (isObject(%obj.controlLineShape))
+			{
+				cancel(%obj.controlLineShape.deleteSchedule);
+				%obj.controlLineShape.drawLine(%obj.getMuzzlePoint(%slot), %hitloc, "0 1 0 0.2", 0.08);
+			}
+			else
+			{
+				%obj.controlLineShape = drawLine(%obj.getMuzzlePoint(%slot), %hitloc, "0 1 0 0.2", 0.08);
+			}
+			%obj.controlLineShape.deleteSchedule = %obj.controlLineShape.schedule(100, delete);
+			%cl.balloon.moveBlimp(%hitloc);
+		}
+	}
+}
