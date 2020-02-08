c1c2844368d82f3376e080e8ef349a6da1b22e8d
ConanODoyle <samuel.w.sze@gmail.com>
2020-02-07 23:46:06 -0800

    Fix datablock image name, add unique projectile datablock for gatling

--- src/blimps/weapons/datablocks.cs
+++ src/blimps/weapons/datablocks.cs
@@ -13,6 +13,83 @@ datablock ItemData(ReferenceTurretItem : hammerItem)
 
 //gatling
 
+
+//bullet trail effects
+datablock ParticleData(GatlingTrailParticle)
+{
+	dragCoefficient      = 3;
+	gravityCoefficient   = -0.0;
+	inheritedVelFactor   = 1.0;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 525;
+	lifetimeVarianceMS   = 55;
+	textureName          = "base/data/particles/dot";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -500.0;
+	spinRandomMax		= 500.0;
+	colors[0]     = "0.9 0.0 0.0 0.8";
+	colors[1]     = "0.9 0.3 0.0 0.6";
+	colors[2]     = "0.9 0.3 0.0 0.0";
+	sizes[0]      = 0.06;
+	sizes[1]      = 0.06;
+	sizes[2]	  = 0.01;
+	times[0]      = 0.0;
+	times[1]	  = 0.9;
+	times[2]      = 1.0;
+
+	useInvAlpha = false;
+};
+
+datablock ParticleEmitterData(GatlingTrailEmitter)
+{
+   ejectionPeriodMS = 8;
+   periodVarianceMS = 0;
+   ejectionVelocity = 0.0;
+   velocityVariance = 0.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 0;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "GatlingTrailParticle";
+};
+
+datablock ProjectileData(GatlingProjectile)
+{
+   projectileShapeName = "./empty.dts";
+   directDamage        = 15;
+   directDamageType    = $DamageType::Default;
+   radiusDamageType    = $DamageType::Default;
+
+   brickExplosionRadius = 0;
+   brickExplosionImpact = true;          //destroy a brick if we hit it directly?
+   brickExplosionForce  = 10;
+   brickExplosionMaxVolume = 15;          //max volume of bricks that we can destroy
+   brickExplosionMaxVolumeFloating = 20;  //max volume of bricks that we can destroy if they aren't connected to the ground
+
+   impactImpulse	     = 400;
+   verticalImpulse	  = 300;
+   explosion           = GunExplosion;
+   particleEmitter     = GatlingTrailEmitter;
+
+   muzzleVelocity      = 180;
+   velInheritFactor    = 1;
+
+   armingDelay         = 00;
+   lifetime            = 4000;
+   fadeDelay           = 3500;
+   bounceElasticity    = 0.5;
+   bounceFriction      = 0.20;
+   isBallistic         = false;
+   gravityMod = 0.0;
+
+   hasLight    = false;
+   lightRadius = 3.0;
+   lightColor  = "0 0 0.5";
+};
+
+//firing smoke
 datablock ParticleData(GatlingSmokeParticle)
 {
 	dragCoefficient      = 3;
@@ -32,6 +109,7 @@ datablock ParticleData(GatlingSmokeParticle)
 
 	useInvAlpha = false;
 };
+
 datablock ParticleEmitterData(GatlingSmokeEmitter)
 {
    ejectionPeriodMS = 3;
@@ -40,7 +118,7 @@ datablock ParticleEmitterData(GatlingSmokeEmitter)
    velocityVariance = 0.1;
    ejectionOffset   = 0.0;
    thetaMin         = 0;
-   thetaMax         = 360;
+   thetaMax         = 180;
    phiReferenceVel  = 0;
    phiVariance      = 360;
    overrideAdvance = false;
@@ -53,16 +131,16 @@ datablock ItemData(GatlingTurretItem : ReferenceTurretItem)
 {
 	shapeFile = "./minigun.dts";
 	uiName = "Gatling";
-	image = "GatlingTurretImageAAAAA";
+	image = "GatlingTurretImage";
 };
 
-datablock ShapeBaseImageData(GatlingTurretImageAAAAA)
+datablock ShapeBaseImageData(GatlingTurretImage)
 {
 	item = GatlingTurretItem;
 	shapeFile = "./minigun.dts";
 	colorShiftColor = "1 1 1 1";
 
-	projectile = gunProjectile;
+	projectile = GatlingProjectile;
 	projectileType = Projectile;
 	spread = 0.001;
 	shellcount = 1;
@@ -106,21 +184,21 @@ datablock ShapeBaseImageData(GatlingTurretImageAAAAA)
 	stateTransitionOnLoaded[5] = "AmmoCheck";
 };
 
-function GatlingTurretImageAAAAA::onMount(%this, %obj, %slot)
+function GatlingTurretImage::onMount(%this, %obj, %slot)
 {	
 	onTurretImageMount(%this, %obj, %slot);
 }
 
-function GatlingTurretImageAAAAA::onReady(%this, %obj, %slot)
+function GatlingTurretImage::onReady(%this, %obj, %slot)
 {	
 }
 
-function GatlingTurretImageAAAAA::onFire(%this, %obj, %slot)
+function GatlingTurretImage::onFire(%this, %obj, %slot)
 {	
 	onTurretImageFire(%this, %obj, %slot);
 }
 
-function GatlingTurretImageAAAAA::onFinishReload(%this, %obj, %slot)
+function GatlingTurretImage::onFinishReload(%this, %obj, %slot)
 {	
 	onTurretImageReload(%this, %obj, %slot);
 	%obj.gunLoaded[%this.getID()] = 20;
